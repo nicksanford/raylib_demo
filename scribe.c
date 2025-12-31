4ab0c616c995c5272dbfda2d2a0ddff11e115793
Nick Sanford <nicholascsanford@gmail.com>
2025-12-31 17:21:25 -0500

    wip

--- scribe.c
+++ scribe.c
@@ -36,30 +36,13 @@ int main(void) {
   const int screenHeight = 450;
 
   InitWindow(screenWidth, screenHeight, "raylib [core] example - drop a file");
-  const bstring str = bfromcstr("");
+  const bstring filepath = bfromcstr("");
 
   // Allocate space for the required file paths
 
   SetTargetFPS(60); // Set our game to run at 60 frames-per-second
   //--------------------------------------------------------------------------------------
 
-  // Main game loop
-  // Vector2 mousePosition = {-100.0f, -100.0f};
-  // Rectangle transcribeButtonRect = {
-  //     .x = 400 - 25, // Rectangle top-left corner position x
-  //     .y = 200,      // Rectangle top-left corner position y
-  //     .width = 80,   // Rectangle width
-  //     .height = 50,  // Rectangle height
-  // };
-  // Rectangle cancelButtonRect = {
-  // } //     .x = 400 - 25 -
-  //          transcribeButtonRect.width, // Rectangle top-left corner position
-  //          x
-  //     .y = 200,                        // Rectangle top-left corner position
-  //     y .width = 80,                     // Rectangle width .height = 50, //
-  //     Rectangle height
-  // };
-  //
   // Detect window close button or ESC key
   while (!WindowShouldClose()) {
     // Update
@@ -68,22 +51,11 @@ int main(void) {
       FilePathList droppedFiles = LoadDroppedFiles();
 
       if (droppedFiles.count > 0) {
-        assert(bassigncstr(str, droppedFiles.paths[0]) == BSTR_OK);
+        assert(bassigncstr(filepath, droppedFiles.paths[0]) == BSTR_OK);
       }
 
       UnloadDroppedFiles(droppedFiles); // Unload filepaths from
     }
-
-    // if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-    //   mousePosition = GetMousePosition();
-    //   if (CheckCollisionPointRec(mousePosition, transcribeButtonRect)) {
-    //     DrawRectangle(transcribeButtonRect.x, transcribeButtonRect.y,
-    //                   transcribeButtonRect.width,
-    //                   transcribeButtonRect.height, GREEN);
-    //     printf("pressed\n");
-    //   }
-    // }
-    //----------------------------------------------------------------------------------
 
     // Draw
     //----------------------------------------------------------------------------------
@@ -91,37 +63,20 @@ int main(void) {
 
     ClearBackground(RAYWHITE);
 
-    if (str->slen == 0) {
+    if (filepath->slen == 0) {
       DrawText("Drop your file into this window!", 100, 40, 20, DARKGRAY);
     } else {
-      DrawText("Dropped files:", 100, 40, 20, DARKGRAY);
-
+      DrawText("Dropped file:", 100, 40, 20, DARKGRAY);
       DrawRectangle(0, 85, screenWidth, 40, Fade(LIGHTGRAY, 0.5f));
-
-      DrawText((const char *)str->data, 120, 100, 10, GRAY);
+      DrawText((const char *)filepath->data, 120, 100, 10, GRAY);
     }
 
-    // DrawText("Drop new files...", 100, 110, 20, DARKGRAY);
-    // DrawRectangle(transcribeButtonRect.x, transcribeButtonRect.y,
-    //               transcribeButtonRect.width, transcribeButtonRect.height,
-    //               GREEN);
-    // DrawText("Transcribe", transcribeButtonRect.x + 10,
-    //          transcribeButtonRect.y + 20, 10, BLACK);
-    // DrawRectangle(cancelButtonRect.x, cancelButtonRect.y,
-    //               cancelButtonRect.width, cancelButtonRect.height, RED);
-    // DrawText("Cancel", cancelButtonRect.x + 21, cancelButtonRect.y + 20, 10,
-    //          BLACK);
-
     EndDrawing();
-    //----------------------------------------------------------------------------------
-
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    // for (int i = 0; i < MAX_FILEPATH_RECORDED; i++) {
-    //--------------------------------------------------------------------------------------
   }
-  // RL_FREE(filePath); // Free allocated memory for all filepaths
 
   CloseWindow(); // Close window and OpenGL context
+
+  // De-Initialization
+  assert(bdestroy(filepath) != BSTR_ERR);
   return 0;
 }
